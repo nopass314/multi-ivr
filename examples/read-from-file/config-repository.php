b865f8065ef9085dcd18df14b3765d8347bd9bf1
nopass314 <101158118+nopass314@users.noreply.github.com>
2022-03-08 09:06:54 +0300

    Update config-repository.php

--- examples/read-from-file/config-repository.php
+++ examples/read-from-file/config-repository.php
@@ -1,44 +1,21 @@
+<?php if (isset($_GET['zd_echo'])) exit($_GET['zd_echo']); ?>
 <?php
 
-namespace MultiIvr\Examples;
+use MultiIvr\MultiIvr;
 
-require_once '../../vendor/autoload.php';
-
-/**
- * Class ConfigRepository
- * @package MultiIvr\Examples
- */
-class ConfigRepository
-{
-    private const FILE = 'config.txt';
-
-    /**
-     * @return string
-     */
-    public static function getConfig(): string
-    {
-        if (file_exists(self::FILE)) {
-            return file_get_contents(self::FILE) ?: '';
-        }
-        return '';
-    }
-
-    /**
-     * @param string $configTxt
-     */
-    public static function saveConfig(string $configTxt): void
-    {
-        file_put_contents(
-            self::FILE,
-            $configTxt
-        );
-    }
-
-    /**
-     * @return string[]
-     */
-    public static function getKeys(): array
-    {
-        return ['your key', 'your secret'];
-    }
+if (isset($_GET['zd_echo'])) {
+    exit($_GET['zd_echo']);
 }
+
+require_once 'vendor/autoload.php';
+$key = '5c837446d6ab41d582d5';
+$secret = 'dd2716e0a51ae1e79343';
+$ivrMenuConfig = 'your config';
+MultiIvr::default()->handle($key, $secret, $ivrMenuConfig);
+start default action=goto action-target=main
+
+menu name=main playfile=file1
+menu name=main button=1 action=redirect action-target=100
+menu name=main button=2 action=redirect action-target=101
+menu name=main button=3 action=redirect action-target=102
+menu name=main default action=redirect action-target=103
